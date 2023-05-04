6384356c3461832c76467add7a2f738aee3081d9
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-05-05 03:15:44 +0530

    Add chat spinner while waiting.

--- frontend/apps/neuranet/neuranetapp/views/chat/js/chat.mjs
+++ frontend/apps/neuranet/neuranetapp/views/chat/js/chat.mjs
@@ -26,6 +26,7 @@ async function send(_element) {
 
     const textareaEdit = document.querySelector("textarea#messagearea"), buttonSendImg = document.querySelector("img#send");
     textareaEdit.classList.add("readonly"); textareaEdit.setAttribute("readonly", "true"); buttonSendImg.src = `${VIEW_PATH}/img/spinner.svg`; 
+    const oldInsertion = _insertAIResponse(userMessageArea, userPrompt);
     const result = await apiman.rest(`${APP_CONSTANTS.API_PATH}/${API_CHAT}`, "POST", 
         {id: session.get(APP_CONSTANTS.USERID), session: [sessionRequest], maintain_session: true, 
             session_id: sessionID}, true);
@@ -34,17 +35,24 @@ async function send(_element) {
     if (!result || (!result.result)) {_showMessage(await i18n.get("ChatAIError")); return;}
 
     sessionID = result.session_id;  // save session ID so that backend can maintain session
+    _insertAIResponse(userMessageArea, userPrompt, result.response, oldInsertion);
+}
+
+function _insertAIResponse(userMessageArea, userPrompt, aiResponse, oldInsertion) {
     const insertionTemplate = document.querySelector("template#chatresponse_insertion_template");   // insert current prompt and reply
-    const newInsertion = insertionTemplate.content.cloneNode(true);
-    newInsertion.querySelector("span#userprompt").textContent = userPrompt;
-    newInsertion.querySelector("span#airesponse").textContent = result.response;
-    document.querySelector("div#chatmainarea").appendChild(newInsertion);
+    const insertion = oldInsertion||insertionTemplate.content.cloneNode(true), 
+        insertionDiv = oldInsertion||insertion.querySelector("div#insertiondiv");
+    insertion.querySelector("span#userprompt").innerHTML = userPrompt;
+    if (aiResponse) insertion.querySelector("span#airesponse").innerHTML = aiResponse;
+    document.querySelector("div#chatmainarea").appendChild(insertion);
     const chatScroller = document.querySelector("div#chatscroller");
     chatScroller.scrollTop = chatScroller.scrollHeight;
 
     document.querySelector("div#start").classList.replace("visible", "hidden"); // hide the startup logo and messages
     chatScroller.classList.replace("hidden", "visible");  // show chats
     userMessageArea.value = ""; // clear text area for the next prompt
+
+    return insertionDiv;
 }
 
 const _showMessage = message => dialog().showMessage(message, "dialog");
