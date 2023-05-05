1bf73e94565c57446adbc4f5997a37300164ab13
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-05-05 15:07:54 +0530

    Enterprise search - add not implemented message.

--- frontend/apps/neuranet/neuranetapp/i18n/i18n_hi.mjs
+++ frontend/apps/neuranet/neuranetapp/i18n/i18n_hi.mjs
@@ -23,5 +23,7 @@ export const i18n = {
 "ViewLabel_chat": "General Chat",
 
 "ErrorConvertingBadInputCode": "Error in conversion, due to bad input code.\n\n{{#message}}{{message}}{{/message}}{{^message}}Code parser failed to parse.{{/message}}\n\nFound at: Line:{{#line}}{{line}}{{/line}}{{^line}}0{{/line}}, Column:{{#column}}{{column}}{{/column}}{{^column}}0{{/column}}.",
-"PossibleErrorConvertingCode": "--- WARNING: Possibly bad code.\n--- {{#message}}{{message}}{{/message}}{{^message}}Code parser failed to parse.{{/message}}\n--- Found at: {{#line}}{{line}}{{/line}}{{^line}}0{{/line}}, Column:{{#column}}{{column}}{{/column}}{{^column}}0{{/column}}."
+"PossibleErrorConvertingCode": "--- WARNING: Possibly bad code.\n--- {{#message}}{{message}}{{/message}}{{^message}}Code parser failed to parse.{{/message}}\n--- Found at: {{#line}}{{line}}{{/line}}{{^line}}0{{/line}}, Column:{{#column}}{{column}}{{/column}}{{^column}}0{{/column}}.",
+
+"NotImplemented": "Not implemented yet."
 }
