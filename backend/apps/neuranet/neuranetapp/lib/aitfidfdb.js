bdc95294fb45a102d037e1e523a2bba8cafef977
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-07-03 18:12:32 +0700

    Add in stemming support and cleanup AI TF.IDF.

--- backend/apps/neuranet/neuranetapp/lib/aitfidfdb.js
+++ backend/apps/neuranet/neuranetapp/lib/aitfidfdb.js
@@ -30,7 +30,16 @@ const EMPTY_DB = {tfidfDocStore: {}, wordDocCounts: {}, vocabulary: []}, INDEX_F
     VOCABULARY_FILE = "vocabulary.json", METADATA_DOCID_KEY="aidb_docid";
 const IN_MEM_DBS = {};
 
-exports.get_tfidf_db = async function(dbPath, metadata_docid_key=METADATA_DOCID_KEY, lang="en", autosave=true, autosave_frequency=500) {
+/**
+ * Creates a new TF.IDF DB instance and returns it. Use this function preferably to get a new DB instance.
+ * @param {string} dbPath The save path for the database
+ * @param {string} metadata_docid_key The document ID key inside document metadata. Default is "aidb_docid"
+ * @param {string} lang The language for the DB. Default is English.
+ * @param {boolean} no_stemming Whether or not to stem the words. Default is to stem. If true stemming won't be used.
+ * @param {boolean} autosave Autosave the DB or not. Default is true.
+ * @param {number} autosave_frequency The autosave frequency. Default is 500 ms. 
+ */
+exports.get_tfidf_db = async function(dbPath, metadata_docid_key=METADATA_DOCID_KEY, lang="en", no_stemming=false, autosave=true, autosave_frequency=500) {
     const normPath = path.resolve(dbPath); if (!IN_MEM_DBS[normPath]) {    // load the DB from the disk only if needed
         try { await fspromises.access(dbPath); } catch (err) {    // check the DB path exists or create it etc.
             if (err.code == "ENOENT") { 
@@ -40,7 +49,7 @@ exports.get_tfidf_db = async function(dbPath, metadata_docid_key=METADATA_DOCID_
         }
         IN_MEM_DBS[normPath] = await exports.loadData(normPath);
     }
-    const db = IN_MEM_DBS[normPath]; db.METADATA_DOCID_KEY = metadata_docid_key;
+    const db = IN_MEM_DBS[normPath]; db.METADATA_DOCID_KEY = metadata_docid_key; db.no_stemming = no_stemming;
 
     const _autosaveIfSelected = _ => {if (autosave) setTimeout(_=>exports.writeData(dbPath, db), autosave_frequency);}
 
@@ -77,24 +86,22 @@ exports.writeData = async (path, db) => {
 }
 
 exports.ingest = exports.create = function(document, metadata, lang="en", db=EMPTY_DB) {
-    const docHash = _getDocumentHashIndex(metadata, lang, db), docWords = _getLangNormalizedWords(document, lang),
+    const docHash = _getDocumentHashIndex(metadata, lang, db), docWords = _getLangNormalizedWords(document, lang, db),
         datenow = Date.now();
-    exports.delete(metadata, lang, db);   // if adding same document, delete the old one first.
-    db.tfidfDocStore[docHash] = {metadata: {...metadata}, scores: {}, length: docWords.length, 
-        date_created: datenow, date_modified: datenow};
+    exports.delete(metadata, lang, db);   // if adding the same document, delete the old one first.
+    const newDocument = {metadata: _deepclone(metadata), scores: {}, length: docWords.length, 
+        date_created: datenow, date_modified: datenow}; db.tfidfDocStore[docHash] = newDocument;
     const wordsCounted = {}; for (const word of docWords) {
         const wordIndex = _getWordIndex(word, db, true); 
         if (!wordsCounted[wordIndex]) {
             db.wordDocCounts[wordIndex] = db.wordDocCounts[wordIndex]?db.wordDocCounts[wordIndex]+1:1; 
             wordsCounted[wordIndex] = true; 
         }
-        db.tfidfDocStore[docHash].scores[wordIndex] = {
-            tfidf: 0, // dummy as we will rebuild the entire index in the next step, as IDF will change with every new doc as the word counts per doc changed (the denominator for IDF)
-            wordcount: db.tfidfDocStore[docHash].scores[wordIndex]?
-                db.tfidfDocStore[docHash].scores[wordIndex].wordcount+1:1 };   
+        if (!newDocument.scores[wordIndex]) newDocument.scores[wordIndex] = {tfidf: 0, wordcount: 1};   // see _recalculateTFIDF below
+        else newDocument.scores[wordIndex].wordcount = newDocument.scores[wordIndex].wordcount+1;   
     }
 
-    _recalculateTFIDF(db);    // rebuild the entire TF.IDF score index
+    _recalculateTFIDF(db);    // rebuild the entire TF.IDF score index for all documents, will fix the 0 scores for this document above too
 }
 
 exports.delete = function(metadata, lang="en", db=EMPTY_DB) {
@@ -113,8 +120,8 @@ exports.delete = function(metadata, lang="en", db=EMPTY_DB) {
 exports.update = (oldmetadata, newmetadata, lang="en", db=EMPTY_DB) => {
     const oldhash = _getDocumentHashIndex(oldmetadata, lang, db), newhash = _getDocumentHashIndex(newmetadata, lang, db),
         document = db.tfidfDocStore[oldhash];
-    document.metadata = {...newmetadata}; document.date_modified = Date.now();
-    db. tfidfDocStore[newhash] = document; delete db. tfidfDocStore[oldhash];
+    document.metadata = _deepclone(newmetadata); document.date_modified = Date.now();
+    db.tfidfDocStore[newhash] = document; delete db.tfidfDocStore[oldhash];
 }
 
 /**
@@ -123,17 +130,17 @@ exports.update = (oldmetadata, newmetadata, lang="en", db=EMPTY_DB) => {
  * d is the document.
  */
 exports.query = (query, topK, filter_function, lang="en", cutoff_score, ignoreCoord=false, db=EMPTY_DB) => {
-    const queryWords = _getLangNormalizedWords(query, lang), scoredDocs = []; let highestScore = 0; 
+    const queryWords = _getLangNormalizedWords(query, lang, db), scoredDocs = []; let highestScore = 0; 
     for (const document of Object.values(db.tfidfDocStore)) {
         if (filter_function && (!filter_function(document.metadata))) continue; // drop docs if they don't pass the filter
         let scoreThisDoc = 0, queryWordsFoundInThisDoc = 0; if (query) for (const queryWord of queryWords) {
             const wordIndex = _getWordIndex(queryWord, db); if (!wordIndex) continue;  // query word not found in the vocabulary
             if (document.scores[wordIndex]) {scoreThisDoc += document.scores[wordIndex].tfidf; queryWordsFoundInThisDoc++;}
         }
-        const coord = ignoreCoord?1:queryWordsFoundInThisDoc/queryWords.length;
-        if (!ignoreCoord) scoreThisDoc = scoreThisDoc*(queryWordsFoundInThisDoc/queryWords.length); // add in coord scoring
-        scoredDocs.push({metadata: document.metadata, score: scoreThisDoc, coord_score: coord, 
-            tfidf_score: scoreThisDoc/coord, query_tokens_found: queryWordsFoundInThisDoc, total_query_tokens: queryWords.length}); 
+        const coordScore = (query && (!ignoreCoord)) ? queryWordsFoundInThisDoc/queryWords.length : 1;
+        scoreThisDoc = scoreThisDoc*coordScore; // add in coord scoring
+        scoredDocs.push({metadata: document.metadata, score: scoreThisDoc, coord_score: coordScore, 
+            tfidf_score: scoreThisDoc/coordScore, query_tokens_found: queryWordsFoundInThisDoc, total_query_tokens: queryWords.length}); 
         if (scoreThisDoc > highestScore) highestScore = scoreThisDoc;
     }
     if (!query) return scoredDocs;  // can't do cutoff, topK etc if no query was given
@@ -149,8 +156,8 @@ exports.query = (query, topK, filter_function, lang="en", cutoff_score, ignoreCo
 }
 
 exports.defragment = function(db) {
-    const newVocabulary = []; for (const [wordIndex,word] of db.vocabulary.entries())
-        if (db.wordDocCounts[wordIndex]) newVocabulary.push(word);
+    const newVocabulary = []; for (const [wordIndex, word] of db.vocabulary.entries())
+        if (db.wordDocCounts[wordIndex]) newVocabulary.push(word);  // rebuild by unsparsing the new vocabulary
     if (newVocabulary.length == db.vocabulary.length) return;   //nothing to do
 
     const newTFIDFDocStore = {}; for (const [documentHash, document] of Object.entries(db.tfidfDocStore)) {
@@ -179,12 +186,30 @@ function _recalculateTFIDF(db) {  // rebuilds the entire TF.IDF index for all do
     }
 }
 
-function _getLangNormalizedWords(document, lang) {     // TODO: Add word stemming here, use natual npm for the stemmer
+function _getLangNormalizedWords(document, lang, db) {     
     // international capable punctuation character regex from: https://stackoverflow.com/questions/7576945/javascript-regular-expression-for-punctuation-international
     const punctuation = /[\$\uFFE5\^\+=`~<>{}\[\]|\u3000-\u303F!-#%-\x2A,-/:;\x3F@\x5B-\x5D_\x7B}\u00A1\u00A7\u00AB\u00B6\u00B7\u00BB\u00BF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u0AF0\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E3B\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65]+/g;
     const words = [], segmenter = new Intl.Segmenter(lang, {granularity: "word"});
-    for (const segmentThis of Array.from(segmenter.segment(document))) if (segmentThis.isWordLike)
-        words.push(segmentThis.segment.replace(punctuation, "").trim().toLocaleLowerCase(lang));
+    const _getStemmer = lang => {
+        const DEFAULT_STEMMER = {stem: word => word};   // null stemmer - not too bad still
+        if (db.no_stemming) return DEFAULT_STEMMER;
+
+        const natural = require("natural");
+        switch (lang) {
+            case "en": return natural.PorterStemmer; 
+            case "es": return natural.PorterStemmerEs;
+            case "jp": return natural.StemmerJa;
+            case "ru": return natural.PorterStemmerRu;
+            case "fr": return natural.PorterStemmerFr;
+            case "de": return natural.PorterStemmerDe;
+            default: return DEFAULT_STEMMER;    
+        }
+    }
+    for (const segmentThis of Array.from(segmenter.segment(document))) if (segmentThis.isWordLike) {
+        const depuntuatedLowerLangWord = segmentThis.segment.replace(punctuation, "").trim().toLocaleLowerCase(lang);
+        const stemmedWord = _getStemmer(lang).stem(depuntuatedLowerLangWord);
+        words.push(stemmedWord);
+    }
     return words;
 }
 
