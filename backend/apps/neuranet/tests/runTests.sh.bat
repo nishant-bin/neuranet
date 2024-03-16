81627b42c15e433e513947437ad3517acb2ac176
TekmonksGitHub <rvkapoor@tekmonks.com>
2024-03-16 18:38:13 +0800

    Moved runTests to main Monkshu

--- backend/apps/neuranet/tests/runTests.sh.bat
+++
@@ -1,7 +0,0 @@
-:; @echo off
-:<<"::CMDLITERAL"
-@node --preserve-symlinks --preserve-symlinks-main --max-old-space-size=8192 .\testMain.js %*
-::CMDLITERAL
-
-:; node --preserve-symlinks --preserve-symlinks-main --max-old-space-size=8192 ./testMain.js $* 
-:; exit
