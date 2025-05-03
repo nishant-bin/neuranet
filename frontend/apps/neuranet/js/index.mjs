f315ce3729c63108fab8095f528567d6bbefe188
TekmonksGitHub <rvkapoor@tekmonks.com>
2025-05-03 17:00:01 +0800

    Move to Monkshu web bundle

---
+++ frontend/apps/neuranet/js/index.mjs
@@ -0,0 +1,14 @@
+/**
+ * Init and run the app. 
+ * (C) 2024 TekMonks. All rights reserved.
+ * See enclosed LICENSE file.
+ */
+
+_init();
+async function _init() {
+    const {webbundlesupport} = await import("/framework/js/webbundlesupport.mjs");    // use monkshu web bundles
+    if (!await webbundlesupport.addWebbundleSupport()) 
+        console.error("Webbundle loading failed or not available. Website performance will be slow.")
+
+    await import("/framework/js/$$.js"); $$.boot(); // now boot the app
+}
